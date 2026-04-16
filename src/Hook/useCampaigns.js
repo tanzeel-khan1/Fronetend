f5d79dabc989415f38b3ffe847da3ec4695694c7
tanzeel-khan1 <tanzeel0680@gmail.com>
2026-04-16 12:32:41 +0500

    ki

--- src/Hook/useCampaigns.js
+++ src/Hook/useCampaigns.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify"; 
 import 'react-toastify/dist/ReactToastify.css';
-
-const API_URL = "https://backend-three-alpha-69.vercel.app/api/campaigns"; 
+  https://backend-git-main-tanzeel0680-6266s-projects.vercel.app/
+// const API_URL = "https://backend-three-alpha-69.vercel.app/api/campaigns"; 
+const API_URL = "https://backend-git-main-tanzeel0680-6266s-projects.vercel.app/api/campaigns"; 
 
 const useCampaigns = () => {
   const [campaigns, setCampaigns] = useState([]);
